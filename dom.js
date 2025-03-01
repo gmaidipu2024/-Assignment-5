c1711c65d594a958f25cdc6996ed2a7792f9d051
Goutam Adhikari Dipu <gtm10322@gmail.com>
2025-03-01 12:58:28 +0600

    complete Board Sucesscully

--- dom.js
+++ dom.js
@@ -11,6 +11,7 @@ document.getElementById("history-clear").addEventListener("click", function(){
     }
 });
 
+
 document.getElementById("complete-task").addEventListener("click", function(){
 
     const countTask = document.getElementById("countTask").innerText;
@@ -20,9 +21,101 @@ document.getElementById("complete-task").addEventListener("click", function(){
    
     const countTask2 = document.getElementById("countTask2").innerText;
     let changeValue2 = parseInt(countTask2);
-    const sum2 = changeValue2 - 1 ;
+    const sum2 = changeValue2 + 1 ;
     document.getElementById("countTask2").innerText = sum2;
     
-  
+    const setDisable = document.getElementById("complete-task")
+    setDisable.setAttribute("disabled", true);
+
+    alert ("Board Updated Successfully")
+})
+
+document.getElementById("complete-task1").addEventListener("click", function(){
+
+    const countTask = document.getElementById("countTask").innerText;
+    let changeValue = parseInt(countTask);
+    const sum = changeValue - 1 ;
+    document.getElementById("countTask").innerText = sum;
+   
+    const countTask2 = document.getElementById("countTask2").innerText;
+    let changeValue2 = parseInt(countTask2);
+    const sum2 = changeValue2 + 1 ;
+    document.getElementById("countTask2").innerText = sum2;
     
+    const setDisable = document.getElementById("complete-task1")
+    setDisable.setAttribute("disabled", true);
+    
+    alert ("Board Updated Successfully")
+})
+
+document.getElementById("complete-task2").addEventListener("click", function(){
+
+    const countTask = document.getElementById("countTask").innerText;
+    let changeValue = parseInt(countTask);
+    const sum = changeValue - 1 ;
+    document.getElementById("countTask").innerText = sum;
+   
+    const countTask2 = document.getElementById("countTask2").innerText;
+    let changeValue2 = parseInt(countTask2);
+    const sum2 = changeValue2 + 1 ;
+    document.getElementById("countTask2").innerText = sum2;
+    
+    const setDisable = document.getElementById("complete-task2")
+    setDisable.setAttribute("disabled", true);
+    
+    alert ("Board Updated Successfully")
+})
+
+document.getElementById("complete-task3").addEventListener("click", function(){
+
+    const countTask = document.getElementById("countTask").innerText;
+    let changeValue = parseInt(countTask);
+    const sum = changeValue - 1 ;
+    document.getElementById("countTask").innerText = sum;
+   
+    const countTask2 = document.getElementById("countTask2").innerText;
+    let changeValue2 = parseInt(countTask2);
+    const sum2 = changeValue2 + 1 ;
+    document.getElementById("countTask2").innerText = sum2;
+    
+    const setDisable = document.getElementById("complete-task3")
+    setDisable.setAttribute("disabled", true);
+    
+    alert ("Board Updated Successfully")
+})
+
+document.getElementById("complete-task4").addEventListener("click", function(){
+
+    const countTask = document.getElementById("countTask").innerText;
+    let changeValue = parseInt(countTask);
+    const sum = changeValue - 1 ;
+    document.getElementById("countTask").innerText = sum;
+   
+    const countTask2 = document.getElementById("countTask2").innerText;
+    let changeValue2 = parseInt(countTask2);
+    const sum2 = changeValue2 + 1 ;
+    document.getElementById("countTask2").innerText = sum2;
+    
+    const setDisable = document.getElementById("complete-task4")
+    setDisable.setAttribute("disabled", true);
+    
+    alert ("Board Updated Successfully")
+})
+
+document.getElementById("complete-task5").addEventListener("click", function(){
+
+    const countTask = document.getElementById("countTask").innerText;
+    let changeValue = parseInt(countTask);
+    const sum = changeValue - 1 ;
+    document.getElementById("countTask").innerText = sum;
+   
+    const countTask2 = document.getElementById("countTask2").innerText;
+    let changeValue2 = parseInt(countTask2);
+    const sum2 = changeValue2 + 1 ;
+    document.getElementById("countTask2").innerText = sum2;
+    
+    const setDisable = document.getElementById("complete-task5")
+    setDisable.setAttribute("disabled", true);
+    
+    alert ("Board Updated Successfully")
 })
